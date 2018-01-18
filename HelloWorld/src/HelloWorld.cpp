e253a0e22297a3f4085bcb9de9ba36898d364b49
Gabriel leal <gaboleal123@gmail.com>
2018-01-18 00:10:13 -0430

    Practica #1
    Practica Hola Mundo

---
+++ HelloWorld/src/HelloWorld.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : Gabriel leal
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << " ¡¡ Hola Mundo !!"<<endl;
+	return 0;
+}
